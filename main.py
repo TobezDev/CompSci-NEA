caf4d796a63c0a02063fd4242c9851481494f9a9
TobezDev <83095177+TobezDev@users.noreply.github.com>
2023-02-02 19:56:43 +0000

    push mysql and base

--- main.py
+++ main.py
@@ -1,279 +1,77 @@
-# from replit import db as Data  ### Replit .env // Remove and replace with ln #2 for VSCode
-import mysql as Data
-
-from color_print import colprint
-from colorama import Fore as col
-
-import random
-import time
-import sys
+import mysql.connector
 import os
 
-#async def sql_connect(Data, key: str, auth: bool):
-#	await Data.connect(
-#		key=f"{key}",
-#		endpoint_url=f"https://{key}.mysql.tobestech.com/?key=vscode.mysql.{key}?auth={str(auth).lower()}",
-#		callback=f"https://{key}.callback.mysql.tobestech.com?key=vscode.mysql.failed.{key}?auth={str(auth).lower()}"
-#	)
-#
-#sql_connect(Data, '', True)
+# Connect to the database
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password=os.getenv("SQL_PASSWORD"),
+    database="adventure_game"
+)
 
+cursor = conn.cursor()
 
-def clear_console():
-	os.system('clear')
-
-
-def player_died(reason: str):
-	colprint('red', "--== YOU DIED ==--")
-	colprint('red',
-	         f"""Your character, {Data['username']} died because of {reason}.""")
-	exit("You died.")
-
-
-if Data['username'] == '' or Data['username'] is None:
-	Data['username'] = f'Default_Username#{random.randint(1,100000)}'
-	# Set a random default username if not already set
-
-username = Data['username']
-
-
-def error(Exception):
-	with Exception as e:
-		print(e)
-
-
-def settings():
-	colprint(
-	 'green', """
-========================
-==      SETTINGS      ==
-========================
-
-Select an option:
- 	1) Change Username
-  	2) Back To Main Menu
-	""")
-	selection = input('>>> ')
-	if selection == '1':
-		colprint('green', f"Your current username is: {username}")
-		colprint(
-		 'green', """
- 		Enter a selection:
- 		1) Change username
-   		2) Exit
-  		""")
-		selection = input('>>> ')
-		if selection == '1':
-			colprint('green', "Enter a new username: ")
-			new_username = input('\n>>> ')
-			Data['username'] = new_username
-			main_menu()
-		elif selection == '2':
-			settings()
-		else:
-			error(Exception)
-	elif selection == '2':
-		main_menu()
-	elif selection == 'admin.getkeys':
-		print(
-		 'red', f"""
-  Key List: {Data.keys()}
-  Total Keys: {len(Data.keys())}
-  Total Storage Space: {int(len(Data.keys()) * 2)} kb
-  		""")
-		settings()
-	else:
-		error(Exception)
-
-
-def main_menu():
-	colprint(
-	 'green', """
-=======================
-=      MAIN MENU      =
-=======================
-
-Select a choice:
-	1) New Game
- 	2) Settings
-  	3) Quit Game
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS inventory (
+    item VARCHAR(255) NOT NULL
+);
 """)
-	selection = input('>>> ')
-	if selection == '1':
-		create_new_game()
-	elif selection == '2':
-		settings()
-	elif selection == '3':
-		quit("Player-requested Exit")
-	else:
-		error(Exception)
-
-
-# generate start of story based on location and difficulty selections
-def generate_story(location: str, difficulty: str):
-	story = f"You are {username}, a brave adventurer embarking on a journey. "
-	if difficulty == 'e':
-		story += "The path ahead is smooth and easy, but you must still be careful as danger lurks around every corner. "
-	elif difficulty == 'm':
-		story += "The path ahead is filled with challenges, but with your skills and determination, you will overcome them. "
-	elif difficulty == 'h':
-		story += "The path ahead is treacherous and difficult, but with your training and perseverance, you will emerge victorious. "
-	elif difficulty == 'i':
-		story += "The path ahead is nearly impossible, but with your courage and strength, you will rise to the challenge."
-	story += f"You begin your journey at the {location} and must defeat the final boss, who awaits your arrival. Good luck, {username}!"
-	colprint('green', story)
-
-
-def create_new_game():
-	Data['player.Username'] = username
-	colprint('green', "Enter a title for this adventure: ")
-	game_title = input('\n>>> ')
-	Data['game.Title'] = game_title
-	locations = ['Hospital', 'School', 'Airport']
-	location_selectors = ['h', 's', 'a']
-	difficulties = ['Easy', 'Medium', 'Hard', 'Insane']
-	difficulty_selectors = ['e', 'm', 'h', 'i']
-	for i in locations:
-		colprint('green', f"{i} - Use {i[0].lower()} to select this option.\n")
-	location = input('>>> ')
-	if location not in location_selectors:  # check if 'location' is valid
-		error(Exception)
-	else:
-		if location == 'h':
-			colprint('green', "Hospital has been selected.\n")
-			location = 'Hospital'
-		elif location == 's':
-			colprint('green', "School has been selected.\n")
-			location = 'School'
-		elif location == 'a':
-			colprint('green', "Airport has been selected.\n")
-			location = 'Airport'
-	for i in difficulties:
-		colprint('green', f"{i} - Use {i[0].lower()} to select this option.\n")
-	difficulty = input('>>> ')
-	if difficulty not in difficulty_selectors:
-		# check if 'difficulty' is valid
-		error(Exception)
-	else:
-		if difficulty == "e":
-			colprint('green', "Easy difficilty has been selected.\n")
-			difficulty = 'Easy'
-		elif difficulty == "m":
-			colprint('green', "Medium difficilty has been selected.\n")
-			difficulty = 'Medium'
-		elif difficulty == "h":
-			colprint('green', "Hard difficilty has been selected.\n")
-			difficulty = 'Hard'
-		elif difficulty == "i":
-			colprint('green', "Insane difficilty has been selected.\n")
-			difficulty = 'Insane'
-
-	Data['difficulty'] = difficulty
-	Data['location'] = location
-
-	input(col.GREEN + """Press ENTER to continue.""")
-	clear_console()
-	colprint(
-	 'green', f"""
-======================= 
-=      NEW  GAME      =
-=======================
-
-Location: {location}
-Difficulty: {difficulty}
-
-Username: {Data['username']}
-
-=======================
-=  IGNORING UPGRADES  =
-= UNTIL UPDATE PUSHED =
-=======================
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS location (
+    place VARCHAR(255) NOT NULL
+);
 """)
+conn.commit()
 
+cursor.execute("INSERT INTO location (place) VALUES ('forest')")
+conn.commit()
 
-# change colour of console output to dark green (classical terminal colour)
-colprint(
- 'green', f"""
-============================
-=          CREDITS         =
-============================
+def get_location():
+    cursor.execute("SELECT place FROM location")
+    return cursor.fetchone()[0]
 
-Developer: Toby
-Tester(s): Ethan, Luke, Adam
-""")
+def update_location(place):
+    cursor.execute("UPDATE location SET place = %s", (place,))
+    conn.commit()
 
-time.sleep(3)
-clear_console()
+def add_item(item):
+    cursor.execute("INSERT INTO inventory (item) VALUES (%s)", (item,))
+    conn.commit()
 
-main_menu()
+def get_inventory():
+    cursor.execute("SELECT item FROM inventory")
+    return [item[0] for item in cursor.fetchall()]
 
-generate_story(Data['location'], Data['difficulty'])
+while True:
+    location = get_location()
+    print("You are in the " + location)
+    print("What would you like to do?")
+    print("1. Move")
+    print("2. Pick up item")
+    print("3. Check inventory")
+    print("4. Quit")
+    choice = int(input("> "))
+    if choice == 1:
+        print("Where would you like to go?")
+        print("1. Forest")
+        print("2. Cave")
+        print("3. Beach")
+        destination = int(input("> "))
+        if destination == 1:
+            update_location("forest")
+        elif destination == 2:
+            update_location("cave")
+        elif destination == 3:
+            update_location("beach")
+    elif choice == 2:
+        item = input("What item would you like to pick up? ")
+        add_item(item)
+    elif choice == 3:
+        inventory = get_inventory()
+        print("You have:")
+        for item in inventory:
+            print(item)
+    elif choice == 4:
+        break
 
-input(col.GREEN + """\nPress ENTER to continue.\n""")
-
-story = f"You are approached by a man, he says he is the head of the {Data['location']}. Do you trust him? (y/n)"
-colprint('green', story)
-
-yn = input(">>> ")
-if yn[0].lower == 'y':
-	if Data['difficulty'][0].lower() == 'e':
-		colprint(
-		 'green',
-		 "He is friendly. He offeres you a drink. You sit with him and discuss the event..."
-		)
-		colprint(
-		 'green',
-		 "The Man: Very strange, this. Only us in this massively popular place, what happened?"
-		)
-		colprint('green', "You: I don't know, seems to be a major event...")
-		colprint('green', "You both start conspiring...")
-	elif Data['difficulty'][0].lower() == 'm':
-		colprint(
-		 'green',
-		 "He is sociable. He offeres you a coffee. You sit with him and discuss the event..."
-		)
-		colprint(
-		 'green',
-		 "The Man: Very strange, this. Only us in this massively popular place, what happened?"
-		)
-		colprint('green', "You: I don't know, seems to be a major event...")
-		colprint('green', "You both start conspiring...")
-	elif Data['difficulty'][0].lower() == 'h':
-		rI = random.randint(1, 2)  # random chance to be killed by the man (50%)
-		if rI == 1:
-			colprint(
-			 'green',
-			 "He is unsociable but, out of shock, offeres you a drink. You sit with him and discuss the event..."
-			)
-			colprint(
-			 'green',
-			 "The Man: Very strange, this. Only us in this massively popular place, what happened?"
-			)
-			colprint('green', "You: I don't know, seems to be a major event...")
-			colprint('green', "You both start conspiring...")
-		elif rI == 2:
-			colprint('orange', "You falsely trusted the man. He was not friendly.")
-			player_died()
-	elif Data['difficulty'][0].lower() == 'i':
-		rI = random.randint(1, 10)  # random chance to be killed by the man (80%)
-		if rI > 2:
-			colprint(
-			 'orange',
-			 "He is not sociable in any way. He offeres you a drink and leaves.")
-			colprint('orange', "You wonder what you did wrong...")
-		else:
-			colprint('orange', "You falsely trusted the man. He was not friendly.")
-			player_died()
-elif yn[0].lower() == 'n':
-	if Data['difficulty'][0].lower() == 'e':
-		#easy
-		pass
-	elif Data['difficulty'][0].lower() == 'm':
-		#med
-		pass
-	elif Data['difficulty'][0].lower() == 'h':
-		#hard
-		pass
-	elif Data['difficulty'][0].lower() == 'i':
-		#insane
-		pass
+conn.close()
